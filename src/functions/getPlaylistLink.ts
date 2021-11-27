e0f63b77721e8f1096e50a75cb515fdaaa032b79
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-11-26 23:11:30 -0300

    chore: add get playlist link function

---
+++ src/functions/getPlaylistLink.ts
@@ -0,0 +1,17 @@
+import prompt from "prompt";
+prompt.start({});
+
+const schema = {
+	properties: {
+		url: {
+			message: "Insert the playlist URL",
+			required: true
+		}
+	}
+};
+
+export const getPlaylistLink = async () => {
+	const response = await prompt.get(schema);
+
+	return response.url;
+};
