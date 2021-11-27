2a17e691e3eff2bcafad1a8b8e8ba656d6bb05dc
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-11-26 23:21:18 -0300

    chore: add environment variables validation

--- src/settings/env.ts
+++ src/settings/env.ts
@@ -3,6 +3,13 @@ dotenv.config();
 
 const { VIDEO_HOLDER_ELEMENT } = process.env;
 
-export const environment = {
+const environment = {
 	videoHolderElement: VIDEO_HOLDER_ELEMENT || null
 };
+
+Object.keys(environment).forEach(key => {
+	if (!environment[key as keyof typeof environment])
+		throw new Error("Invalid environment property value.");
+});
+
+export default environment;
