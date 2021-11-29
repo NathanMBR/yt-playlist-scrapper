2a948f2b5c6b65e9efc83b3ef9fa0da1eb48ed60
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-11-29 20:07:48 -0300

    chore: add roll reference to environment variables

--- src/settings/env.ts
+++ src/settings/env.ts
@@ -2,10 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 new Boolean("false");
-const { VIDEO_HOLDER_ELEMENT, HIDE_BROWSER } = process.env;
+const { VIDEO_HOLDER_ELEMENT, ROLL_REFERENCE, HIDE_BROWSER } = process.env;
 
 const environment = {
 	videoHolderElement: VIDEO_HOLDER_ELEMENT || null,
+	rollReference: ROLL_REFERENCE?.replace("$hashtag$", "#") || null,
 	hideBrowser: HIDE_BROWSER === "true"
 };
 
