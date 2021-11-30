233d52302de40659479b30fcc0d65e11da990b8d
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-11-29 21:03:10 -0300

    chore: add roll page until end function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,3 +1,4 @@
 export * from "./getPlaylistLink.js";
 export * from "./getEndOfPageElement.js";
 export * from "./getVideoListElements.js";
+export * from "./rollPageUntilEnd.js";
---
+++ src/functions/rollPageUntilEnd.ts
@@ -0,0 +1,24 @@
+import puppeteer from "puppeteer";
+import { getEndOfPageElement, getVideoListElements } from ".";
+import { awaitTime } from "./helpers";
+
+export const rollPageUntilEnd = async (page: puppeteer.Page) =>
+	await page.evaluate(async () => {
+		const endOfPage = await getEndOfPageElement(page);
+		let videoList = await getVideoListElements(page);
+
+		let previousLoadVideoQuantity = 0;
+		let laterLoadVideoQuantity = videoList.length;
+
+		/* eslint-disable no-await-in-loop */
+		while (previousLoadVideoQuantity < laterLoadVideoQuantity) {
+			previousLoadVideoQuantity = laterLoadVideoQuantity;
+
+			endOfPage.scrollIntoView({ block: "end" });
+			await awaitTime(4000);
+			videoList = await getVideoListElements(page);
+
+			laterLoadVideoQuantity = videoList.length;
+		}
+		/* eslint-enable no-await-in-loop */
+	});
