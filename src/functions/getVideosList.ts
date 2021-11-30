521738f7ad5978f0dc50582e6da9f33cd56a3b60
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-11-30 20:50:20 -0300

    fix: map link array to return href properties at get videos list function

--- src/functions/getVideosList.ts
+++ src/functions/getVideosList.ts
@@ -4,7 +4,7 @@ import { environment } from "../settings/index.js";
 export const getVideosList = async (page: Page) =>
 	await page.evaluate(
 		(videoHolderElementEval: string) =>
-			[...document.querySelectorAll(`${videoHolderElementEval} a#thumbnail`)],
+			[...document.querySelectorAll<HTMLAnchorElement>(`${videoHolderElementEval} a#thumbnail`)].map(anchor => anchor.href),
 
 		environment.videoHolderElement
 	);
