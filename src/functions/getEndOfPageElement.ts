32c1dedbd3a26d034cf6e32de918908423d89501
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-11-29 23:27:25 -0300

    fix: edit get elements functions

--- src/functions/getEndOfPageElement.ts
+++ src/functions/getEndOfPageElement.ts
@@ -1,14 +1,17 @@
 import puppeteer from "puppeteer";
 import environment from "../settings/env.js";
 
-export const getEndOfPageElement = async (page: puppeteer.Page) =>
-	await page.evaluate(async () => {
-		await page.waitForSelector(environment.rollReference as string);
+export const getEndOfPageElement = async (page: puppeteer.Page) => {
+	await page.waitForSelector(environment.rollReference as string);
 
-		const endOfPage = document.querySelector(environment.rollReference as string);
+	return await page.evaluate(
+		async () => {
+			const endOfPage = document.querySelector(environment.rollReference as string);
 
-		if (!endOfPage)
-			throw new Error("Roll reference element not found.");
+			if (!endOfPage)
+				throw new Error("Roll reference element not found.");
 
-		return endOfPage;
-	});
+			return endOfPage;
+		}
+	);
+};
--- src/functions/getVideoListElements.ts
+++ src/functions/getVideoListElements.ts
@@ -1,14 +1,17 @@
 import puppeteer from "puppeteer";
 import environment from "../settings/env.js";
 
-export const getVideoListElements = async (page: puppeteer.Page) =>
-	await page.evaluate(async () => {
-		await page.waitForSelector(environment.videoHolderElement as string);
+export const getVideoListElements = async (page: puppeteer.Page) => {
+	await page.waitForSelector(environment.videoHolderElement as string);
 
-		const videoList = document.querySelectorAll(environment.videoHolderElement as string);
+	return await page.evaluate(
+		async () => {
+			const videoList = document.querySelectorAll(environment.videoHolderElement as string);
 
-		if (videoList.length <= 0)
-			throw new Error("The list is probably empty.");
+			if (videoList.length <= 0)
+				throw new Error("The list is probably empty.");
 
-		return [...videoList];
-	});
+			return [...videoList];
+		}
+	);
+};
