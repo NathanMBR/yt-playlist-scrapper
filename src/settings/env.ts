b5f6452632b82a57f86e3987b0609430dec2fef9
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2021-11-30 23:21:29 -0300

    chore: add xpath environment variables

--- src/settings/env.ts
+++ src/settings/env.ts
@@ -2,11 +2,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 new Boolean("false");
-const { VIDEO_HOLDER_ELEMENT, ROLL_REFERENCE, HIDE_BROWSER } = process.env;
+const {
+	VIDEO_HOLDER_ELEMENT,
+	ROLL_REFERENCE,
+	HIDE_BROWSER,
+	SHOW_MENU_XPATH,
+	LOAD_HIDDEN_VIDEOS_XPATH
+} = process.env;
 
 const environment = {
 	videoHolderElement: VIDEO_HOLDER_ELEMENT || null,
 	rollReference: ROLL_REFERENCE?.replace("$hashtag$", "#") || null,
+	showMenuXPath: SHOW_MENU_XPATH || null,
+	loadHiddenVideosXPath: LOAD_HIDDEN_VIDEOS_XPATH || null,
 	hideBrowser: HIDE_BROWSER === "true"
 };
 
